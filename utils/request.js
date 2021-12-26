2108d743d2f9226384fd0c30825524778e5c9b7d
anirban00537 <anirban00537@gmail.com>
2021-12-26 12:20:45 +0600

    updated

--- utils/request.js
+++ utils/request.js
@@ -1,14 +1,11 @@
 import axios from "axios";
 import Cookies from "js-cookie";
-// https://galactic-escape-356951.postman.co/workspace/My-Workspace~e1dff801-3bf1-4e67-86fc-e4c665489247/request/12582813-ebca030a-13e7-46d5-8f2f-4d84a5f801f4
 
 const service = axios.create({
-  baseURL:
-    "https://galactic-escape-356951.postman.co/workspace/My-Workspace~e1dff801-3bf1-4e67-86fc-e4c665489247/request/",
+  baseURL: "http://161.35.10.61/api/",
 
   headers: {
-    "Content-Type": "application/json",
-    "X-Requested-With": "XMLHttpRequest",
+    Accept: "application/json",
   },
 });
 
