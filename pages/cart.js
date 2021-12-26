3eb554050f3001cbb14ca50ebdfcd0b7294b0e14
anirban00537 <anirban00537@gmail.com>
2021-12-27 00:37:36 +0600

    cart

--- pages/cart.js
+++ pages/cart.js
@@ -2,53 +2,72 @@ import { Flex, Text, Button, useColorModeValue } from "@chakra-ui/react";
 import CartItems from "../components/cards/cartItems";
 import { useSelector } from "react-redux";
 import { ssrAuthCheck } from "../middleware/authCheck";
+import { useEffect, useState } from "react";
 
 const cart = () => {
   const myItem = useSelector((state) => state.cart.cart);
-  console.log(myItem, "all items");
-  return (
-    <div>
+  const [cartStatus, setcartStatus] = useState(false);
+
+  useEffect(() => {
+    myItem?.items?.length > 0 && setcartStatus(true);
+  }, [myItem]);
+
+  if (!cartStatus) {
+    return (
       <Flex
-        align="center"
-        justify="flex-start"
-        direction="column"
-        w="90%"
-        h="100vh"
-        margin={8}
+        align={"center"}
+        justify={"center"}
+        height={["100vh"]}
+        bg={useColorModeValue("gray.50", "gray.800")}
       >
-        {myItem?.items?.map((item, i) => (
-          <CartItems key={i} item={item} />
-        ))}
+        <Text>No items in cart</Text>
+      </Flex>
+    );
+  } else {
+    return (
+      <div>
         <Flex
           align="center"
-          justify="space-between"
-          direction="row"
-          w="60%"
-          bg="white"
-          padding={8}
-          borderRadius="lg"
-          border={`1px solid ${useColorModeValue("gray.200", "gray.300")}`}
-          boxShadow="lg"
+          justify="flex-start"
+          direction="column"
+          w="90%"
+          h="100vh"
+          margin={8}
         >
-          <Text fontSize="lg" fontWeight="bold">
-            Total
-          </Text>
-          <Text fontSize="lg" fontWeight="bold">
-            {myItem?.gross_total}$
-          </Text>
-          <Button
-            size="lg"
-            height="48px"
-            width="200px"
-            border="2px"
-            borderColor="green.500"
+          {myItem?.items?.map((item, i) => (
+            <CartItems key={i} item={item} />
+          ))}
+          <Flex
+            align="center"
+            justify="space-between"
+            direction="row"
+            w="60%"
+            bg="white"
+            padding={8}
+            borderRadius="lg"
+            border={`1px solid ${useColorModeValue("gray.200", "gray.300")}`}
+            boxShadow="lg"
           >
-            Checkout
-          </Button>
+            <Text fontSize="lg" fontWeight="bold">
+              Total
+            </Text>
+            <Text fontSize="lg" fontWeight="bold">
+              {myItem?.gross_total}$
+            </Text>
+            <Button
+              size="lg"
+              height="48px"
+              width="200px"
+              border="2px"
+              borderColor="green.500"
+            >
+              Checkout
+            </Button>
+          </Flex>
         </Flex>
-      </Flex>
-    </div>
-  );
+      </div>
+    );
+  }
 };
 
 export default cart;
